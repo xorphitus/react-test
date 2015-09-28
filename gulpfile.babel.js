dd45cb034a9540462bcbe757b3f34a1d2f8c743b
xorphitus <xorphitus@gmail.com>
2015-09-29 00:21:50 +0900

    主に ESLint の設定

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,16 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import eslint from 'gulp-eslint';
 
 gulp.task('default', () => {
-    return gulp.src('gulp/assets/javascripts/app.js')
-        .pipe(babel())
-        .pipe(gulp.dest('app/assets/javascripts/components'));
+  return gulp.src('gulp/assets/javascripts/app.jsx')
+    .pipe(babel())
+    .pipe(gulp.dest('app/assets/javascripts/components'));
+});
+
+gulp.task('lint', () => {
+  return gulp.src(['gulp/assets/javascripts/app.jsx'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
